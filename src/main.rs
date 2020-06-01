5283074dcfe0353b669b288a36dd4c00a068544f
Eóin McMahon <eoin.mcmahon.dev@gmail.com>
2020-06-01 15:42:58 +0100

    changed to clap.rs for arg parsing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::env;
+use clap::{Arg, App};
 mod lib;
 use std::collections::HashMap;
 
@@ -6,18 +6,45 @@ use std::collections::HashMap;
 const API_URL: &str = "https://api.github.com/repos/toptal/gitignore/contents/templates?ref=master";
 
 fn main() {
-    // collect command line arguments
-    let args: Vec<String> = env::args().collect();
+    let matches = App::new("IgnoriGen")
+                    .version("1.0")
+                    .author("Eoin McMahon <eoin.mcmahon.dev@gmail.com>")
+                    .about("Grabs gitignore templates from gitignore.io")
+                    .arg(Arg::with_name("LANGUAGE(S)")
+                         .short("l")
+                         .long("lang")
+                         .takes_value(true)
+                         .multiple(true)
+                         .required(true)
+                         .help("template(s) to generate gitignore for i.e Rust, Flutter, VsCode etc."))
+                    .arg(Arg::with_name("DESTINATION")
+                        .short("d")
+                        .long("dest")
+                        .help("Destination to store the gitignore file in")
+                        .takes_value(true))
+                    .get_matches();
+
+    let languages: Vec<&str>= matches.values_of("LANGUAGE(S)").unwrap().collect();
+    let destination: &str = matches.value_of("DESTINATION").unwrap_or("./");
+
+    println!("Generating .gitignore files for: {:?}", languages);
+    println!("Storing .gitignore in: {:?}", destination);
 
     // perform a get request to list the gitignore repository files
     let repo_contents: String = lib::http_get(API_URL);
     let file_map: HashMap<String, String> = lib::build_file_map(&repo_contents);
 
+    let mut gitignore_template: String = String::new();
+
     // ignore first argument
-    for language in args[1..].iter() {
+    for language in languages.iter() {
         // get gitignore template for each CLA
-        println!("Language requested: {}", language);    
-        let ignore_file: String = lib::get_ignore_file(&file_map, language);
-        println!("{}", ignore_file);
+        let ignore_body: String = lib::get_ignore_file(&file_map, language);
+        let ignore_template: String = format!("{} gitignore \n=================== \n\n {} \n\n", language.to_uppercase(), ignore_body);
+        gitignore_template.push_str(&ignore_template);
     }
+
+    println!("{}", gitignore_template);
+
+
 }
