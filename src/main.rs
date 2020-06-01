405e1e2446b15edcca5bd19ff05cfeb367aad88c
Eóin McMahon <eoin.mcmahon.dev@gmail.com>
2020-06-01 01:06:22 +0100

    cleaned up code, added http get request function and added some comments

--- src/main.rs
+++ src/main.rs
@@ -8,13 +8,13 @@ struct FileRes {
     download_url: Option<String>,
 }
 
-fn extract_from_response(res: &str) -> HashMap<String, Option<String>> {
+fn build_file_map(res: &str) -> HashMap<String, Option<String>> {
     let mut file_map: HashMap<String, Option<String>> = HashMap::new();
 
     // parse json response to extract name and download link
     let all_files: Vec<FileRes> = serde_json::from_str(res).unwrap();
 
-    // filter out non gitignore files
+    // filter out non-gitignore files
     let gitignore_files: Vec<&FileRes> = all_files
         .iter()
         .filter(|file| file.name.contains("gitignore"))
@@ -26,19 +26,24 @@ fn extract_from_response(res: &str) -> HashMap<String, Option<String>> {
       file_map.insert(name, file.download_url.clone());
     }
 
-
     return file_map;
 }
 
+// performs a http GET request using the reqwest crate
+fn http_get(url: &str) -> String {
+    let response = reqwest::get(url)
+        .expect("Error getting file")
+        .text()
+        .expect("Could not parse Text");
+
+    return response;
+}
 
 fn main() {
     // github api get request to list the gitignore repository files
-    let repo_contents = reqwest::get("https://api.github.com/repos/github/gitignore/contents")
-        .expect("Could not get available files list error")
-        .text()
-        .expect("no text");
-    
-    let file_map: HashMap<String, Option<String>> = extract_from_response(&repo_contents);
+    let repo_contents: String = http_get("https://api.github.com/repos/github/gitignore/contents");
+
+    let file_map: HashMap<String, Option<String>> = build_file_map(&repo_contents);
 
     println!("{:?}", file_map)
 }
