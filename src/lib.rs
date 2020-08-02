02e05c3cc55028b4d00abcbd6cfea4c2e17a56a0
Hans Pistor <hpistor@fastmail.com>
2020-08-01 21:58:39 -0400

    Remove extraneous globstars
    It is possible for path prefix & .gitignore line to start with a globstar. This change removes all but one globstar.

--- src/lib.rs
+++ src/lib.rs
@@ -99,8 +99,15 @@ fn format_gitignore(raw_body: &String, prefix_path: Option<&Path>, language: &st
                     line.to_string()
                 };
 
+                let mut corrected_line_path = Path::new(&corrected_line);
+                while path.ends_with("**/") && corrected_line_path.starts_with("**/") {
+                    corrected_line_path = corrected_line_path
+                        .strip_prefix("**/")
+                        .expect("This shouldn't fail because we check if '**/' exists first");
+                }
+
                 body.push_str(
-                    path.join(Path::new(&corrected_line))
+                    path.join(corrected_line_path)
                         .to_str()
                         .expect("Unknown path found in gitignore."),
                 );
@@ -133,7 +140,7 @@ pub fn generate_gitignore_file(languages: Vec<&str>, file_map: &HashMap<String, 
 
     // generate gitignore for each language and append to output string
     for path_and_language in languages.iter() {
-        // Split the path and language, with path being None if the language name doesn't contain a 
+        // Split the path and language, with path being None if the language name doesn't contain a
         // '/'
         let last_slash_index = path_and_language.rfind('/');
         let language = &path_and_language[last_slash_index.map_or(0, |x| x + 1)..];
@@ -217,7 +224,11 @@ pub fn write_to_file(dest: &str, gitignore: String) -> std::io::Result<()> {
     let filepath: PathBuf = Path::new(dest).join(".gitignore");
     println!(
         "Writing file to {}... ✏️ ",
-        filepath.to_str().expect("Unknown output file name.").bright_blue().bold()
+        filepath
+            .to_str()
+            .expect("Unknown output file name.")
+            .bright_blue()
+            .bold()
     );
     let mut file = File::create(filepath)?;
     file.write_all(gitignore.as_bytes())?;
@@ -239,7 +250,11 @@ pub fn append_to_file(destination: &str, gitignore: String) -> std::io::Result<(
     if !combined.is_empty() {
         println!(
             "Loaded existing gitignore file from {} 💾",
-            filepath.to_str().expect("Unknown file path.").bright_blue().bold()
+            filepath
+                .to_str()
+                .expect("Unknown file path.")
+                .bright_blue()
+                .bold()
         );
 
         // write it to file
